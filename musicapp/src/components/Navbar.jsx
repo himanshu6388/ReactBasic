44ad39c8e13faaa642632b1e5d8a25a1bd11c00b
himanshu6388 <himanshu219181@gmail.com>
2024-12-09 20:06:05 +0530

    updated

--- musicapp/src/components/Navbar.jsx
+++ musicapp/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-
+import styles from './style.module.css'
 function Navbar({data}){
     return(
         <>
             <div className="w-full px-10 py-3 flex justify-between items-center">
-                <h3>Orange</h3>
+                <h3 className={[styles.a]}>Songs</h3>
                 <div className="flex p-4 bg-orange-600 text-white rounded-md text-sm gap-3 ">
                     <h3>Favorites</h3>
                     <h4>{data.filter(item=>item.added).length}</h4>
